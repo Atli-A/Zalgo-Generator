17415d5bb380468d5b4e63202f398b62b7230062
Atli Arnarsson <atli.nikulas@gmail.com>
2021-01-14 19:56:33 -0800

    output box works again

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let zalgoList = ["&#771", "&#772", "&#770", "&#773", "&#774", "&#775", "&#817", "&#863"];
-let inpt;
-let slideVal;
+
 let slider = document.getElementById("slide")
 let inputBox = document.getElementById("inputArea")
+let outputBox = document.getElementById("outputArea")
 
-
-slider.addEventListener("input", function() {
-  addZalgo(document.getElementById("inputArea").value)
+slider.addEventListener("input", function () {
+    getZalgo(document.getElementById("inputArea").value)
 })
 
-inoutBox.addEventListener("change", function() {
-  addZalgo(document.getElementById("inputArea").value)
+inputBox.addEventListener("input", function () {
+    outputArea.innerHTML = getZalgo(document.getElementById("inputArea").value)
 })
 
 
@@ -23,22 +22,23 @@ inoutBox.addEventListener("change", function() {
 //     slideVal = document.getElementById("slide").value;
 //     //console.log(inpt);
 
-//     document.getElementById("outputArea").innerHTML = addZalgo(inpt);
+//     document.getElementById("outputArea").innerHTML = getZalgo(inpt);
 //   }
 // }
 
-function addZalgo(inptVal) {
-  let s = inptVal;
-  let finalString = "";
-  for (let j = 0; j < s.length; j++) {
-    finalString += s.charAt(j);
-    for (let i = 0; i < document.getElementById("slide").value; i++) {
-      finalString =
-        finalString +
-        zalgoList[Math.round(Math.random() * (zalgoList.length - 1))];
+function getZalgo(inptVal) {
+    console.log("getZalgo() called")
+    let s = inptVal;
+    let finalString = "";
+    for (let j = 0; j < s.length; j++) {
+        finalString += s.charAt(j);
+        for (let i = 0; i < document.getElementById("slide").value; i++) {
+            finalString =
+                finalString +
+                zalgoList[Math.round(Math.random() * (zalgoList.length - 1))];
+        }
     }
-  }
-  return finalString;
+    return finalString;
 }
 
 // function loop() {
